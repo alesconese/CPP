2f83a939c2da482d48c42093495a8d7973b20ca9
Ales De Toledo Delgado <ade-tole@car4s1.42barcelona.com>
2024-08-17 20:10:56 +0200

    header

--- CPP-Module-01/ex00/Zombie.hpp
+++ CPP-Module-01/ex00/Zombie.hpp
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   Zombie.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ade-tole <ade-tole@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/08/17 20:09:13 by ade-tole          #+#    #+#             */
+/*   Updated: 2024/08/17 20:09:38 by ade-tole         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef ZOMBIE_HPP
 # define ZOMBIE_HPP
 
@@ -7,13 +19,13 @@
 class Zombie
 {
 	private:
-			std::string	_name;
+		std::string	_name;
 	
 	public:
-			Zombie(std::string name);
-			~Zombie();
-			
-			void	announce();
+		Zombie(std::string name);
+		~Zombie();
+		
+		void	announce();
 };
 
 #endif
