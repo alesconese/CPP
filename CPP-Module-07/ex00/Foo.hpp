ca1e34829b4012d9b7e4caddc60f613ce867ecdb
Ales De Toledo Delgado <ade-tole@cbr4s4.42barcelona.com>
2025-02-20 19:24:19 +0100

    M07 ex00

---
+++ CPP-Module-07/ex00/Foo.hpp
@@ -0,0 +1,32 @@
+#ifndef FOO_HPP
+# define FOO_HPP
+
+# include <iostream>
+
+template	<typename T>
+void	swap(T &x, T &y)
+{
+	T	z = x;
+	x = y;
+	y = z;
+}
+
+template	<typename T>
+T	min(T x, T y)
+{
+	if (x < y)
+		return x;
+	else
+		return y;
+}
+
+template	<typename T>
+T	max(T x, T y)
+{
+	if (x > y)
+		return x;
+	else
+		return y;
+}
+
+#endif
