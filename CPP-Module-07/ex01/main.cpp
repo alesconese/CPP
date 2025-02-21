e80aee1c950280cfbd6c1940951a1c4968329fed
alesconese <alesdetoledo@gmail.com>
2025-02-21 18:48:45 +0100

    M07 ex01

--- CPP-Module-07/ex01/main.cpp
+++ CPP-Module-07/ex01/main.cpp
@@ -1,25 +1,48 @@
 #include "Iter.hpp"
 
-void	add(int &x)
+void	ft_add(int &x)
 {
 	x += 1;
 }
 
+void	ft_changeCase(char &c)
+{
+	if (c >= 65 && c <= 90)
+		c += 32;
+	else if (c >= 97 && c <= 122)
+		c -= 32;
+}
+
 int	main()
 {
-	int	iarr[5] = {23, 42, 101, 9, 11};
-	
-	std::cout << "* Original: { ";
-	for (int i = 0; i < 5; i++)
-		std::cout << iarr[i] << " ";
-	std::cout << "}" << std::endl;
+	{
+		std::cout << "* * * * * TEST 1: INT ARRAY * * * * *" << std::endl;
+		
+		int	iarr[5] = {23, 42, 101, 9, 11};
+		
+		std::cout << "* Original: { ";
+		for (int i = 0; i < 5; i++)
+			std::cout << iarr[i] << " ";
+		std::cout << "}" << std::endl;
 
-	::iter<int>(iarr, 5, add);
-	
-	std::cout << "* Modified: { ";
-	for (int i = 0; i < 5; i++)
-		std::cout << iarr[i] << " ";
-	std::cout << "}" << std::endl;
+		::iter<int>(iarr, 5, ft_add);
+		
+		std::cout << "* Modified: { ";
+		for (int i = 0; i < 5; i++)
+			std::cout << iarr[i] << " ";
+		std::cout << "}" << std::endl;
+	}
+	{
+		std::cout << std::endl << "* * * * * TEST 2: CHAR ARRAY * * * * *" << std::endl;
+		
+		std::string	str = "Hi my name is Slim Shady";
+		char	*carr = (char *)str.c_str();
 
+		std::cout << "* Original: " << carr << std::endl;
+
+		::iter(carr, str.size(), ft_changeCase);
+
+		std::cout << "* Modified: " << carr << std::endl;
+	}
 	return 0;
 }
