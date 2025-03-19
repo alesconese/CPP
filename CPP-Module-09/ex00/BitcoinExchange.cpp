bd076461163f5f0d58e14f1a505c70cfc62f06d2
alesconese <alesdetoledo@gmail.com>
2025-03-19 23:32:20 +0100

    M09 ex00 [wip]

--- CPP-Module-09/ex00/BitcoinExchange.cpp
+++ CPP-Module-09/ex00/BitcoinExchange.cpp
@@ -4,28 +4,7 @@ BitcoinExchange::BitcoinExchange()
 {
 	std::cout << "* BitcoinExchange default constructor called." << std::endl;
 	
-	std::ifstream	data_file;
-
-	data_file.open("data.csv");
-	if (data_file.fail())
-	{
-		std::cout << "(!) ERROR: Unable to open data file.\n";
-		return;
-	}
-
-	std::string	line;
-
-	std::getline(data_file, line);
-	if (line != "date,exchange_rate")
-	{
-		std::cout << "(!) ERROR: Invalid data file format.\n";
-		return ;
-	}
-	
-	while (!data_file.eof())
-	{
-		std::getline(data_file, line, ',');
-	}
+	loadData();
 }
 BitcoinExchange::~BitcoinExchange()
 {
@@ -42,7 +21,80 @@ BitcoinExchange	&BitcoinExchange::operator=(BitcoinExchange const &src)
 {
 	std::cout << "* BitcoinExchange copy assignment operator called" << std::endl;
 	if (this != &src)
-		_rate = src._rate;
+		_historic = src._historic;
 	
 	return *this;
+}
+
+void	BitcoinExchange::loadData()
+{
+	std::ifstream	data_file;
+	std::string		date;
+	std::string		value;
+
+	data_file.open("data.csv");
+	if (data_file.fail())
+	{
+		std::cout << "(!) ERROR: Unable to open data file.\n";
+		return;
+	}
+
+	std::getline(data_file, date);
+	if (date != "date,exchange_rate")
+	{
+		std::cout << "(!) ERROR: Invalid data file format.\n";
+		return ;
+	}
+	
+	while (std::getline(data_file, date))
+	{
+		value = date.substr(date.find(',') + 1);
+		date = date.substr(0, date.find(','));
+
+		//debug
+		//std::cout << "[read] date: " << date << " value: " << value << std::endl;
+
+		//verify correct date and value format.
+		_historic[date] = std::atof(value.c_str());
+	}
+	data_file.close();
+}
+
+bool	BitcoinExchange::checkValidDate(const tm &tm) const
+{
+	switch (tm.tm_mon)
+	{
+		case 0:
+		case 2:
+		case 4:
+		case 6:
+		case 7:
+		case 9:
+		case 11:
+			return (tm.tm_mday <= 31);
+		case 3:
+		case 5:
+		case 8:
+		case 10:
+			return (tm.tm_mday <= 30);
+		case 1:
+			if (tm.tm_year % 4 == 0 && (tm.tm_year % 100 != 0 || tm.tm_year % 400 == 0))
+				return (tm.tm_mday <= 29);
+			else
+				return (tm.tm_mday <= 28);
+		default:
+			return (false);
+	}
+}
+
+float	BitcoinExchange::getValue(std::string const &date) const
+{
+	std::map<std::string, float>::const_iterator	it;
+
+	it = _historic.find(date);
+	if (it == _historic.end())
+	{
+		throw std::runtime_error("Invalid date.");
+	}
+	return (it->second);
 }
--- CPP-Module-09/ex00/BitcoinExchange.hpp
+++ CPP-Module-09/ex00/BitcoinExchange.hpp
@@ -4,17 +4,23 @@
 # include <iostream>
 # include <fstream>
 # include <map>
+# include <cstdlib>
 
 class	BitcoinExchange
 {
 	private:
-			std::map<std::string, float>	_rate;
+			std::map<std::string, float>	_historic;
+
+			void	loadData();
 
 	public:
 			BitcoinExchange();
 			~BitcoinExchange();
 			BitcoinExchange(BitcoinExchange const &src);
 			BitcoinExchange	&operator=(BitcoinExchange const &src);
+
+			float	getValue(std::string const &date) const;
+			bool	checkValidDate(const tm &tm) const;
 };
 
 #endif
